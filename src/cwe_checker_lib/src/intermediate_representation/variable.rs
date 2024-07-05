f1b5f4c496f4837c39cbbde791c9fec206c00aef
Valentin Obst <valentin.obst@fkie.fraunhofer.de>
2024-07-05 23:14:36 +0200

    lib/ir/variable: add methods for Subregister Substitution

--- src/cwe_checker_lib/src/intermediate_representation/variable.rs
+++ src/cwe_checker_lib/src/intermediate_representation/variable.rs
@@ -29,3 +29,23 @@ impl Display for Variable {
         Ok(())
     }
 }
+
+impl Variable {
+    pub fn is_physical_register(&self) -> bool {
+        !self.is_temp
+    }
+
+    pub fn is_unnamed_subregister(&self) -> bool {
+        self.name.starts_with("$R_")
+    }
+
+    pub fn name_to_offset(&self) -> Option<u64> {
+        if self.is_unnamed_subregister() || !self.is_physical_register() {
+            let offset = self.name.split('_').last().unwrap().to_string();
+
+            Some(u64::from_str_radix(offset.strip_prefix("0x").unwrap(), 16).unwrap())
+        } else {
+            None
+        }
+    }
+}
