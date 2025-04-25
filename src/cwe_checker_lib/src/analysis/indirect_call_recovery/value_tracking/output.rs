045c6207bb5aa9ba3131a3ab6acabd0480938372
Felix Ulonska <git@webfoo.de>
2025-04-25 23:44:14 +0200

    set ind_call to null from ghidra

--- src/cwe_checker_lib/src/analysis/indirect_call_recovery/value_tracking/output.rs
+++ src/cwe_checker_lib/src/analysis/indirect_call_recovery/value_tracking/output.rs
@@ -34,6 +34,7 @@ impl IndirectCalls {
             .par_iter_mut()
             .for_each(|blk| {
                 let Some(targets) = self.indirect_calls.get(&Blk(blk.tid.clone().into())) else {
+                    blk.set_ind_call_targets(vec![]);
                     return;
                 };
                 blk.set_ind_call_targets(targets.iter().map(|func| func.tid.clone()));
