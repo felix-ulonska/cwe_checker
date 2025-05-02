d3db4e42815c8c21971834c592da6f9c38c04899
Felix Ulonska <git@webfoo.de>
2025-05-03 00:48:37 +0200

    removed: rule times

--- src/cwe_checker_lib/src/analysis/indirect_call_recovery/value_tracking/mod.rs
+++ src/cwe_checker_lib/src/analysis/indirect_call_recovery/value_tracking/mod.rs
@@ -166,7 +166,7 @@ impl From<&Mloc> for Loc {
 
 //ascent_par! {
 ascent_par! {
-    #![measure_rule_times]
+    //#![measure_rule_times]
     //#![generate_run_timeout]
     // ID is for tracking
     relation assign_reg(Reg, Exp, Arc<Tid>);
